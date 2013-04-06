76c74d5babdc666260b10b095768eafe05c7de45
Ramnique Singh <mail@ramniquesingh.name>
2013-04-06 15:28:32 +0530

    Write api to store and generate id
    Signed-off-by: Ramnique Singh <mail@ramniquesingh.name>

--- api/application/controllers/entries.php
+++ api/application/controllers/entries.php
@@ -9,4 +9,21 @@ class Entries_Controller extends Base_Controller
 	{
 		echo 'Hello earth.';
 	}
+
+	public function post_add()
+	{
+		$entry = new Entry(Input::get());
+
+		if ($entry->validate()) {
+			if ($entry->save()) {
+				return Response::json(array(
+					'id' => $entry->id
+				), 201);
+			} else {
+				return Response::error(500);
+			}
+		} else {
+			return Response::json($entry->errors()->all(), 400);
+		}
+	}
 }
